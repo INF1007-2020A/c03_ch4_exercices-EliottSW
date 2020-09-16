cdb82580f262f928b9a83e345ba838b867fc1586
Eliott Schuhmacher-Wantz <eliott.schuhmacher-wantz@polymtl.ca>
2020-09-16 11:02:48 -0400

    exercice 3 bon

--- exercice.py
+++ exercice.py
@@ -11,7 +11,8 @@ def remove_third_char(string: str) -> str:
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return string.find("w"), string.replace("w", 'z')
+    old_char_index = string.find(old_char)
+    return string[:old_char_index] + new_char + string[old_char_index+1:]
 
 
 def get_nb_char(string: str, char: str) -> int:
